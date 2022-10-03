c18412e691a2adcda945b64c7219858139621654
parth2714 <ilpasalm@gmail.com>
2022-10-03 15:30:19 +0530

    final

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ btnRoll.addEventListener("click", function () {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 2. Display dice
+    // 2. Display dices
     diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
 
